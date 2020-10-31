e6a023d32c93aaf564bafc55eb7ca5576bca1fc6
Abdeldjallil HADJI <32374382+djilousp@users.noreply.github.com>
2020-10-31 13:24:14 +0100

    Revert "handle complete todos"

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Todo from "./Todo"
-const Todolist = ({todos, handleDeleteTodos, handleCompleteTodos}) => {
+const Todolist = ({todos, handleDeleteTodos}) => {
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map(todo => (
-                    <Todo 
-                        key={todo.id} 
-                        id={todo.id} 
-                        todo={todo} 
-                        handleDeleteTodos={handleDeleteTodos} 
-                        handleCompleteTodos={handleCompleteTodos}
-                    />
+                    <Todo key={todo.id} id={todo.id} todo={todo} handleDeleteTodos={handleDeleteTodos}/>
                 ))}
                 
             </ul>
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,20 +1,11 @@
 import React from "react"
 import Form from "../Form"
 import Todolist from "../Todolist"
-const Wrapper = ({ todos, inputText, handleUserInput , handleSubmitTodos, handleDeleteTodos, handleCompleteTodos, filter}) => {
+const Wrapper = ({ todos, inputText, handleUserInput , handleSubmitTodos, handleDeleteTodos, filter}) => {
     return (
         <div className="wrapper">
-            <Form 
-                handleUserInput={ handleUserInput } 
-                inputText={inputText} 
-                handleSubmitTodos={ handleSubmitTodos } 
-                filter={filter}
-            />
-            <Todolist 
-                todos={todos} 
-                handleDeleteTodos={handleDeleteTodos} 
-                handleCompleteTodos={handleCompleteTodos}
-            />
+            <Form handleUserInput={ handleUserInput } inputText={inputText} handleSubmitTodos={ handleSubmitTodos } filter={filter}/>
+            <Todolist todos={todos} handleDeleteTodos={handleDeleteTodos} />
         </div>
     );
 }
